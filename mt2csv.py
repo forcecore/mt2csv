d3d84bf345c050075bab5e94c1a34f7a751e9d78
ForceCore <ssanzing@gmail.com>
2020-05-07 23:45:56 +0900

    Modernize mt2csv with Pandas

--- mt2csv.py
+++ mt2csv.py
@@ -1,115 +1,76 @@
 #!/usr/bin/env python
 
 import sys
-import operator
+import pandas as pd
 
-class Mt2csv :
-    def __init__( self ) :
-        self.data = {}  # holds parsed data
-            # data{ data_name } = array of values
-        self.headers = [] # collect headers in order because dict has its own ordering.
+
+class Mt2csv:
+    def __init__(self):
+        self.df = None
         self.simulator = ""
         self.title = ""
 
-    def read_tokens( self, f ) :
-        result = []
-        for line in f :
-            toks = line.split()
-            result.extend( toks )
-        return result
+    def yield_tokens(self, f):
+        for line in f:
+            for tok in line.split():
+                yield tok
 
-    # modifies self.headers, self.data
-    def parse_tokens( self, tokens ) :
-        # fsm state description
-        HEADER = 0
-        VALUES = 1
+    def read_header(self, tokens):
+        """
+        tokens: iterator that yields tokens. (Must not be a list!)
+        """
+        header = []
+        for tok in tokens:
+            header.append(tok)
+            if tok.find('#') >= 0:
+                break
+        return header
 
-        mode = HEADER
-        header_len = 0
-        i = 0
-        for tok in tokens :
-            # find '#'.
-            if mode == HEADER :
-                self.headers.append( tok )
-                self.data[ tok ] = [] # allocate array
-                if tok.find( '#' ) >= 0 :
-                    mode = VALUES
-                    header_len = len( self.headers )
-            else : # VALUE mode
-                head = self.headers[ i ]
-                arr = self.data[ head ]
-                arr.append( float( tok ) )
-                i = (i+1) % header_len
-                # print( head, tok )
+    def yield_rows(self, tokens, header_len):
+        # If you have .alter in spice, you get multiple measurements.
+        # In other words, you get multiple rows.
+        # optimize.mt0 is an example of this case.
+        row = []
+        for tok in tokens:
+            row.append(float(tok))
+            if len(row) >= header_len:
+                yield row
+                row = []
+        assert len(row) == 0, "Left-over data detected"
 
-    def read( self, fname ) :
-        f = open( fname )
-        self.simulator = f.readline().strip()
-        self.title = f.readline().strip()
+    def parse_tokens(self, f):
+        """
+        Updates self.data.
+        f: stream object
+        """
+        tokens = self.yield_tokens(f)
+        header = self.read_header(tokens)
+        df = pd.DataFrame(columns=header,
+                          data=self.yield_rows(tokens, len(header)))
+        return df
 
-        tokens = self.read_tokens( f )
-        self.parse_tokens( tokens )
+    def read(self, fname):
+        with open(fname) as f:
+            self.simulator = f.readline().strip()
+            self.title = f.readline().strip()
+            self.df = self.parse_tokens(f)
 
-    # func should be one of "max" or "min".
-    def get_minmax( self, func, prefix ) :
-        ns = []
-        vals = []
-
-        for (n, val) in self.data.items() :
-            if n.startswith( prefix ) :
-                vals.extend( val )
-                ns.append( n )
-
-        # by default, max is picked.
-        if func == "min" :
-            index, value = min( enumerate( vals ), key=operator.itemgetter(1) )
-        else :
-            index, value = max( enumerate( vals ), key=operator.itemgetter(1) )
-
-        return ( ns[ index ], value )
-    
-    def get_avg( self, prefix ) :
-        vals = []
-
-        for (n, val) in self.data.items() :
-            if n.startswith( prefix ) :
-                vals.extend( val )
-
-        return sum( vals ) / len( vals )
-    
-    def emit( self ) :
-        print( self.simulator )
-        print( self.title )
-
-        # emit header
-        print( ",".join( self.headers ) )
-
-        # now for the data...
-        # check data sanity
-        data_len = -1
-        for h in self.headers :
-            arr = self.data[ h ]
-            if data_len == -1 :
-                data_len = len( arr )
-            else :
-                # print( data_len, len( arr ) )
-                assert data_len == len( arr )
-
-        # emit data, now!
-        for i in range(0, data_len) :
-            row = []
-            for h in self.headers :
-                arr = self.data[ h ]
-                row.append( str( arr[ i ] ) )
-            print( ",".join( row ) )
-
+    def to_csv(self, f):
+        """
+        f: stream, str, path object or anything that works with df.to_csv()
+        """
+        print(self.simulator)
+        print(self.title)
+        self.df.to_csv(f)
 
 
 ###
 ### main
 ###
-if __name__ == "__main__" :
+if __name__ == "__main__":
     fname = sys.argv[1]
+    ofname = sys.argv[2]
     mt2csv = Mt2csv()
-    mt2csv.read( fname )
-    mt2csv.emit()
+    mt2csv.read(fname)
+    mt2csv.to_csv(ofname)
+    print("Wrote", ofname)
